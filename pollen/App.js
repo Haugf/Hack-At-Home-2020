d0f752d879d24b7474e9cf4f8b34ee207b948ba6
Travis Raghubans <traghubans@traviss-mbp.home>
2020-05-02 23:31:17 -0400

    Stack navigation updates

---
+++ pollen/App.js
@@ -0,0 +1,38 @@
+//import statements
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/login';
+
+// import Map from './Map';
+
+// intializting routing stack
+const Stack = createStackNavigator();
+
+// how our app routes
+function App() 
+{
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF7F50',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
